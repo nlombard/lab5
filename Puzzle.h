b27a62ffd95aa148edd19d15d3dbf7c901ce29de
Nicholas Lombardo <nlombard@nd.edu>
2015-02-23 22:30:23 -0500

    coding

--- Puzzle.h
+++ Puzzle.h
@@ -6,16 +6,51 @@
 #ifndef PUZZLE_H
 #define PUZZLE_H
 #include <iostream>
-
+#include <vector>
+#include <fstream>
 using namespace std;
 
-friend ostream& operator<<(ostream& output, Puzzle& myPuzzle);
 
 class Puzzle{
+	
+	friend ostream& operator<<(ostream&, Puzzle&);
+
 	public:
 		Puzzle(); //constructor
 		
 	private:
-		vector <vector <int>> Board;
+		vector<vector <int> > Board;
+};
+
+//Function Implementations
+Puzzle::Puzzle(){ //constructor
+	int value, i = 0, j = 0;
+	ifstream infile;
+	vector<int> rowVec;
+	infile.open("samplePuzzle.txt");
+	for (int i = 0; i < 9; i++){
+		for( int j = 0; j < 9; j++) {
+			infile >> value;
+			rowVec.push_back(value);
+		}
+		Board.push_back(rowVec);
+		rowVec.clear();
+	}
+}
+
+ostream& operator<<(ostream& output, Puzzle& myPuzzle){
+	cout << "    ";
+	for (int k = 1; k < 10; k++){
+		cout << k << " ";
+	}
+	cout << endl << "    - - - - - - - - -" <<  endl;
+	for (int i = 0; i < 9; i++){
+		cout << i + 1 << " | ";
+		for( int j = 0; j < 9; j++) {
+			output << myPuzzle.Board[i][j] << " ";
+		}
+			cout << endl;
+	}
+	return(output);
 }
 #endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+//main.cpp
+//main program for Sudoku
+//Nicholas Lombardo
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Puzzle.h"
+using namespace std;
+
+int main(){
+Puzzle myPuzzle;
+cout << myPuzzle;
+}
