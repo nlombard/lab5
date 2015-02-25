cfdcebcc20b0072d4ed3784b0ac8d0ca509af287
Nicholas Lombardo <nlombard@nd.edu>
2015-02-24 21:44:23 -0500

    lab 5 work

--- mainSudoku.cpp
+++ mainSudoku.cpp
@@ -5,13 +5,13 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <stdlib.h>
 #include "Puzzle.h"
 using namespace std;
 
 int main(){
 Puzzle<int> mySudoku("samplePuzzle.txt");
-Puzzle<char> myWordoku("wordoky.txt");
-mySudoku.displayBoard();
-cout << endl << endl; // space between boards
-myWordoku.displayBoard();
+system("clear");
+cout << "Welcome to Sudoku the game, have fun!" << endl;
+mySudoku.play();
 }
